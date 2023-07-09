4a71db7cc368578f64d9366a50d59c0e13c3c228
Gagan Rewala <gagan.rewala@zycus.com>
2023-07-09 16:44:26 +0530

    Day 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms'
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -16,7 +18,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { FormGroup,FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  public registerForm !: FormGroup;
+  constructor(private formBuilder:FormBuilder,private http:HttpClient,private router:Router){}
 
+  ngOnInit():void{
+    this.registerForm = this.formBuilder.group({
+      fullname:[''],
+      email:[''],
+      password:[''],
+      moile:['']
+    })
+  }
+  register(){
+    alert("register working")
+    console.log(this.registerForm.value)
+  }
 }
